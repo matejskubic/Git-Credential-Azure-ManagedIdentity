68674a94a7b68cfd02afeb093b7df566c091290a
matejskubic <839965+matejskubic@users.noreply.github.com>
2025-01-19 21:57:35 +0100

    improve error handling in Git credential retrieval script with detailed messages

--- Git-Credential-Azure-ManagedIdentity.sh
+++ Git-Credential-Azure-ManagedIdentity.sh
@@ -2,9 +2,9 @@
 
 # Function to download jq to a temporary location
 download_jq() {
-    local jq_temp="/tmp/jq"
+    local jq_temp="$HOME/jq"
     if [ ! -f "$jq_temp" ]; then
-        curl -L -o "$jq_temp" "https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64"
+        curl -s -L -o "$jq_temp" "https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64"
         chmod +x "$jq_temp"
     fi
     echo "$jq_temp"
@@ -15,8 +15,25 @@ get_access_token() {
     local resource=$1
     local query_param=$2
     local jq_path=$(download_jq)
-    local token_response=$(curl -s "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=${resource}${query_param}" -H "Metadata: true")
-    echo $(echo $token_response | "$jq_path" -r .access_token)
+    # https://learn.microsoft.com/en-us/entra/identity/managed-identities-azure-resources/how-to-use-vm-token#get-a-token-using-http
+    token_response=$(curl -s --fail-with-body "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=${resource}${query_param}" -H "Metadata: true" -H "Accept: application/json")
+    local exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        local error_description=$(echo $token_response | "$jq_path" .error_description)
+        echo $error_description
+        echo "Error: Failed to get token response: $error_description" >&2
+        echo $token_response >&2
+        exit 2
+    fi
+
+    local access_token=$(echo $token_response | "$jq_path" -r .access_token)
+    if [ -z "$access_token" ]; then
+        echo "Error: Access token is empty" >&2
+        echo "$token_response" >&2
+        exit 3
+    fi
+
+    echo $access_token
 }
 
 # Function to get the credentials for Git
@@ -34,18 +51,23 @@ if [ "$1" == "get" ]; then
     while IFS= read -r line; do
         if [[ $line == username=* ]]; then
             value="${line#username=}"
-            if [[ $value == /subscription/* ]]; then
+            if [[ $value == /subscriptions/* ]]; then
                 query_param="&msi_res_id=$value"
             elif [[ ${#value} -eq 36 ]]; then
                 query_param="&client_id=$value"
             else
-                query_param="&username=$value"
+                query_param="&$value"
             fi
         fi
     done
 
     access_token=$(get_access_token $resource "$query_param")
-    get_git_credentials $access_token
+    exit_code=$?
+    if [ $exit_code -eq 0 ]; then
+        get_git_credentials "$access_token"
+    else
+        exit $exit_code
+    fi
 else
     echo "Missing or unsupported command: '$1'"
     exit 1
